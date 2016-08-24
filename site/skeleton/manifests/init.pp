655b0eb0029bf5e40aab76c9dcd4c7c08159eb50
tdanner01 <tdanner01@gmail.com>
2016-08-24 13:25:27 -0700

    manage file resource
    Exercise 9.3

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,19 @@
 class skeleton {
-	  	file { '.bashrc':
-		  ensure => present
-		  path   => /etc/skel
-		}
+  	file { 'bashrc':
+	  ensure => file,
+	  path   => /etc/skel/.bashrc,
+	  owner	 => 'root',
+	  group  => 'root',
+	  mode   => '0644',
+	  source => 'puppet:///modules/skeleton/bashrc
 	}
+
+	file { 'skel'
+	  ensure => directory,
+	  path   => '/etc/skel',
+	  owner	 => 'root',
+	  group  => 'root',
+	  mode   => '0755',
+	}
+		
+}
