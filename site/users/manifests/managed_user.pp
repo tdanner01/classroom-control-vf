a2cffe73f2bcb38a81b210499dde7f4f20714879
tdanner01 <tdanner01@gmail.com>
2016-08-25 13:42:37 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,19 +1,20 @@
 define user::managed_user (
   $group = $title,
-)
-  {
+) 
+{
   user { $title:
     ensure => present,
   }
-  file { "/home/${title}:
-    ensure +> directory,
-    owner => $title,
-    group => $group,
-    
+  
+  file { "/home/${title}":
+    ensure  => directory,
+    owner   => $title,
+    group   => $group,
   }
-  file { "/ho,me/$title/.ssh":
-    ensure => directory,
-    owner
-    group => $group,
-    mode
+  
+  file { "/home/${title}/.ssh":
+    ensure  => directory,
+    owner   => $title, 
+    group   => $group,
+    mode    => '0700',
 }
