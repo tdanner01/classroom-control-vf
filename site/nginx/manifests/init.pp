b4b1e716587efb84bcad496942b227078338b93c
tdanner01 <tdanner01@gmail.com>
2016-08-25 08:28:14 -0700

    initial commit
    exercise 11.2 Managing nginx

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,35 +3,46 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
-  
-  file { '/etc/nginx/ngnix.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
-  
+
   file { '/var/www':
     ensure  => directory,
     owner   => 'root',
     group   => 'root',
-    mode    => '0644',
-  }
-
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+    mode    => '0755',
   }
   
+  file { 'index.html':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/var/www/index.html',
+    source  => 'puppet:///modules/nginx/index.html',
+  }
 
-  service { 'memcached':
+  file { 'ngnix.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/etc/nginx/ngnix.conf',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+  file { 'default.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/etc/nginx/conf.d/default.conf',
+    source  => 'puppet:///modules/nginx/default.conf',
+  }
+
+  service { 'ngnix.conf':
     ensure => running,
     enable => true,
-    subscribe => File['/etc/sysconfig/memcached'],
+    subscribe => File['/etc/nginx/ngnix.conf'],
   }
 
 }
