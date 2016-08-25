196b80bc354571709d101e966f9f322121196ab2
tdanner01 <tdanner01@gmail.com>
2016-08-25 10:12:32 -0700

    implement Resource Deffaults
    exercise 13.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,27 +4,23 @@ class nginx {
     ensure => present,
   }
 
-  file { '/var/www':
-    ensure  => directory,
+  File {
     owner   => 'root',
     group   => 'root',
-    mode    => '0755',
+    mode    => '0644',
+    
+  file { '/var/www':
+    ensure  => directory,
   }
   
   file { 'index.html':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     path    => '/var/www/index.html',
     source  => 'puppet:///modules/nginx/index.html',
   }
 
   file { 'nginx.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     path    => '/etc/nginx/nginx.conf',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
@@ -32,9 +28,6 @@ class nginx {
 
   file { 'default.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     path    => '/etc/nginx/conf.d/default.conf',
     source  => 'puppet:///modules/nginx/default.conf',
   }
